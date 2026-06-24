e41a30f5a4e029b51b1a9401409789a63f9ee986
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-06-24 18:44:30 +0200

    Activar aplicación modular Sprint 0

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import SprintZeroApp from './SprintZeroApp.tsx'
+import SprintZeroAppV2 from './SprintZeroAppV2.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <SprintZeroApp />
+      <SprintZeroAppV2 />
     </BrowserRouter>
   </StrictMode>,
 )
